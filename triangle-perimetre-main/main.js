6f012a8be91e99e459c6b676bf7373b42ecd0099
BIGJUICYAPE <113376487+bigjuicyape@users.noreply.github.com>
2023-09-11 14:22:16 -0600

    ee

--- triangle-perimetre-main/main.js
+++ triangle-perimetre-main/main.js
@@ -1,1 +1,29 @@
-// Triangle Perimeter Assignment Start Code
+let xA = 0;
+let yA = 0;
+let xB = 0;
+let yB = 0;
+let xC = 0;
+let yC = 0;
+let AB = 0;
+let AC = 0;
+let BC = 0;
+document.getElementById("btn").addEventListener("click", calculate);
+
+function calculate() {
+  // getting values
+  xA = document.getElementById("xA").value;
+  yA = document.getElementById("yA").value;
+  xB = document.getElementById("xB").value;
+  yB = document.getElementById("yB").value;
+  xC = document.getElementById("xC").value;
+  yC = document.getElementById("yC").value;
+  // calculating length
+  AB = Math.sqrt((xA - xB) ** 2 + (yA - yB) ** 2);
+  AC = Math.sqrt((xA - xC) ** 2 + (yA - yC) ** 2);
+  BC = Math.sqrt((xB - xC) ** 2 + (yB - yC) ** 2);
+  // output
+  document.getElementById("AB").innerHTML = AB;
+  document.getElementById("AC").innerHTML = AC;
+  document.getElementById("BC").innerHTML = BC;
+  document.getElementById("Perimeter").innerHTML = AB + AC + BC;
+}
